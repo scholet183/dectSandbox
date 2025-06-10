0e45ee3d98607038f90469ac16a5ad416b535140
Tim Schöler <tim_schoeler@baxter.com>
2025-06-10 08:12:58 +0200

    added pigpio for controlling pins

--- cmake/deps_cpm.cmake
+++ cmake/deps_cpm.cmake
@@ -11,3 +11,9 @@ CPMAddPackage(
     "CATCH_BUILD_EXTRA_TESTS OFF"
     "CATCH_BUILD_FUZZERS OFF"
 )
+
+CPMAddPackage(
+    NAME pigpio
+    GITHUB_REPOSITORY "joan2937/pigpio"
+    GIT_TAG master
+)
